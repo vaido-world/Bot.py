e6595b2f4dd033e633cc05bee2f648773818e222
Feldwor <BoQsc@users.noreply.github.com>
2022-11-07 17:07:00 +0200

    Trial to make Environment Variables work in Linux

---
+++ install_discord.py.sh
@@ -0,0 +1,2 @@
+# Download and Install Discord.py Module
+python3 -m pip install -U discord.py
--- start_bot.sh
+++ start_bot.sh
@@ -1,2 +1,31 @@
-nohup python3"./bot.py" > my.log 2>&1 &
-echo $! > save_pid.txt
+# Remember to chmod this file for execution: X (0755)
+# /etc/profile.d
+
+# cat ~/.discord_bot_pid.txt
+
+# Before running, Kill the bot if already running.
+FILE="~/.discord_bot_pid.txt"
+if test -f "$FILE"; then
+	kill -9 `cat $FILE`
+	rm "$FILE"
+fi
+
+nohup python3 "./bot.py" > bot.log 2>&1 &
+
+# Sets Process ID to User Environment Variable
+# export discord_bot_pid="$!"
+# echo $discord_bot_pid
+
+# Save Process ID to Text File.
+echo $!
+echo $! > ~/.discord_bot_pid.txt
+
+pid=$!
+sleep 3
+ps --pid "$pid" >/dev/null
+if [ "$?" -eq 0 ]; then
+    echo "PID $pid exists and is running."
+else
+    echo "PID $pid does NOT exist."
+	cat "./bot.log"
+fi
