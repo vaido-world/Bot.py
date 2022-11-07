eb99aadac55361fa7081eb8f5a6617aa6bec98f1
Feldwor <BoQsc@users.noreply.github.com>
2022-11-07 18:27:20 +0200

    Barebones of start_stop for Linux

--- start_stop_bot.sh
+++ start_stop_bot.sh
@@ -29,3 +29,19 @@ else
     echo "PID $pid does NOT exist."
 	cat "./bot.log"
 fi
+
+
+# List all exported Environment Variables of Current Shell 
+# export -p 
+# env
+# printenv
+
+# Kill the bot
+FILE="~/.discord_bot_pid.txt"
+if test -f "$FILE"; then
+	kill -9 `cat $FILE`
+	rm "$FILE"
+fi
+
+#kill -9 `cat save_pid.txt`
+#rm save_pid.txt
